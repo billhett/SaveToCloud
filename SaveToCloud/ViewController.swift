76d2f59033002aeaad7f8dadcc7e3689a38b64b3
William Hettinger <bill.hettinger@gmail.com>
2018-03-04 13:12:32 -0500

    set up alert box for adding notes

--- SaveToCloud/ViewController.swift
+++ SaveToCloud/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func onPlusTapped() {
+        let alert = UIAlertController(title: "Thpe Something", message: "What would you like to save in a note", preferredStyle: .alert)
+        
+        alert.addTextField { (textField) in
+            textField.placeholder = "Type note here"
+            
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let post = UIAlertAction(title: "Post", style: .default) { (_) in
+            guard let text = alert.textFields?.first?.text else { return }
+            print("text is : \(text)")
+        }
+        alert.addAction(cancel)
+        alert.addAction(post)
+        present(alert, animated: true, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -23,3 +42,19 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Blank"
+        return cell
+    }
+}
